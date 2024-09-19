7d771284d6c073609d53ac20f7a1b2b5b3a4395e
Jah-ziere <146846048+Jah-ziere@users.noreply.github.com>
2024-09-19 12:20:11 -0400

    Challenge 1

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,7 +8,7 @@ public class FollowPlayer : MonoBehaviour
     private Vector3 offset = new Vector3(0, 5, -8);
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         transform.position = player.transform.position + offset;
     }
